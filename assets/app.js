0d5abb216588ee02d7029920d564ef525673e99c
Christian Rémillard <christian.remillard@gmail.com>
2022-05-16 11:57:46 -0400

    Le menu contextuel des fichiers se ferme lorsqu'on clique ailleurs.

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,7 @@ var fileTypes = {
   '.mp3': 'audio'
 }
 
-var dots = '<span class="options" onclick="stopPropagation"><svg style="width:24px;height:24px" viewBox="0 0 24 24" class="options-icon"><path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" /></svg></span>'
+var dots = '<span class="options"><svg style="width:24px;height:24px" viewBox="0 0 24 24" class="options-icon"><path fill="currentColor" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" /></svg></span>'
 
 function isDir(el) {
   return $(el).find('> *:first').text().endsWith('/')
@@ -26,13 +26,26 @@ function isMedia(el) {
   return typeof fileTypes[ext] !== 'undefined'
 }
 
-function stopPropagation(ev) {
-  console.log('stop...')
-  ev.stopPropagation();
+function openOptions(el) {
+  if ($('.options-container').length === 1) {
+    return
+  }
+  $(el).append('<ul class="options-container shadow"><li>allo</li><li>toto</li><li></li><li></li><li></li><li></li></ul>')
+  $(document).on('mouseup', documentOnMouseup);
 }
 
-function openOptions(el) {
-  $(el).append('<ul class="options-container shadow"><li>allo</li><li>toto</li><li></li><li></li><li></li><li></li></ul>')
+function closeOptions($el) {
+  $el.remove();
+  $(document).off('mouseup')
+}
+
+// Toggle show/hide typeahead on click outside
+function documentOnMouseup(e) {
+  var $container = $('.options-container');
+  // if the target of the click isn't the container nor a descendant of the container
+  if (!$container.is(e.target) && $container.has(e.target).length === 0) {
+    closeOptions($container)
+  }
 }
 
 $(function () {
