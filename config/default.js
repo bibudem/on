5632ae7d1608e2af7d09ce937c20a170f6b3d995
Martin Sévigny <m.sevigny@umontreal.ca>
2022-05-17 13:18:46 -0400

    Ajout de l'extension .tif dans la liste des images.

--- config/default.js
+++ config/default.js
@@ -7,7 +7,7 @@ module.exports = {
   key: null,
   baseDir: 'd:\\projets\\on\\public\\data\\cs',
   smallImageMaxSize: 2 * 1024 * 1024, // 2Mb
-  extImages: [".jpg", ".jpeg", ".png", ".webp", ".svg", ".gif", ".tiff"], // file browser
+  extImages: [".jpg", ".jpeg", ".png", ".webp", ".svg", ".gif", ".tiff", ".tif"], // file browser
   fileTypes: {
     '.jpg': 'image',
     '.jpeg': 'image',
