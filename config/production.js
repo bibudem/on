ccc2071e08a0691182cbe6bff0342aa3db4e35be
Christian Rémillard <christian.remillard@gmail.com>
2022-05-25 10:39:36 -0400

    Ajout du fichier de config/production.js

---
+++ config/production.js
@@ -0,0 +1,8 @@
+module.exports = {
+  server: {
+    port: 8001,
+    origin: 'https://oncs.bib.umontreal.ca'
+  },
+  baseDir: '/DATA/oncs/data',
+  iiifImageServerURL: 'https://oncs.bib.umontreal.ca/iiif/3/',
+}
