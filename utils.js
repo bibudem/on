8b184659386933f7c95a24b304949212663553e9
Martin Sévigny <m.sevigny@umontreal.ca>
2022-06-03 06:34:23 -0400

    Fin du test et finalisation de l'ouverture au public.

--- utils.js
+++ utils.js
@@ -83,9 +83,11 @@ module.exports.setContentType = function(res) {
     }
 }
 
+// Indique si un usager peut modifier le contenu
+// Retourne true si c'est du campus UdeM
 module.exports.userCanEdit = function(r) {
     let origin = r.get("X-Forwarded-For");
     if (!origin) origin = r.ip;
-    if (origin.startsWith("132.204") || origin.startsWith("10.")  || origin.startsWith("107.159.41.17")) return true;
+    if (origin.startsWith("132.204") || origin.startsWith("10.")) return true;
     else return false;
 }
